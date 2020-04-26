8a0939df4f9f135373d66dc683cd685238c31574
Rácz András <raczandras0204@gmail.com>
2020-04-26 14:30:12 +0200

    Added the print and correctprint functions. The correctprint function displays what the user should see during the game, while the print function displays every information about the objects for debug purposes.

--- src/main/java/gamelogic/Main.java
+++ src/main/java/gamelogic/Main.java
@@ -25,8 +25,9 @@ public class Main {
         }
 
         randomizeMines(field);
-        
 
+        print();
+        
     }
 
     private static void selectDifficulty(){
@@ -54,6 +55,96 @@ public class Main {
         }
   }
 
+  private static void print(){
+        int rownum = 0;
+        int colnum = 0;
+
+        System.out.print("  ");
+
+        for( int i = 0; i < numberOfFields; i++){
+            if(colnum <= 10) {
+                System.out.print(" " + colnum);
+            }
+            else{
+                System.out.print(colnum);
+            }
+            colnum++;
+        }
+
+      System.out.print("\n  ");
+
+      for( int i = 0; i < numberOfFields*2; i++){
+          System.out.print("_");
+      }
+      System.out.println("");
+
+        for(int i = 0; i< numberOfFields; i++){
+            if(rownum < 10){
+                System.out.print(rownum+ " |");
+            }
+            else {
+                System.out.print(rownum+"|");
+            }
+            rownum++;
+
+            for( int j = 0; j< numberOfFields; j++){
+                if(field[i][j].isMine()) {
+                    System.out.print("M ");
+                }
+                else{
+                    System.out.print(field[i][j].getNeighborMines()+" ");
+                }
+            }
+            System.out.println("");
+        }
+  }
+
+    private static void correctPrint(){
+        int rownum = 0;
+        int colnum = 0;
+
+        System.out.print("  ");
+
+        for( int i = 0; i < numberOfFields; i++){
+            if(colnum <= 10) {
+                System.out.print(" " + colnum);
+            }
+            else{
+                System.out.print(colnum);
+            }
+            colnum++;
+        }
+
+        System.out.print("\n  ");
+
+        for( int i = 0; i < numberOfFields*2; i++){
+            System.out.print("_");
+        }
+        System.out.println("");
+
+        for(int i = 0; i< numberOfFields; i++){
+            if(rownum < 10){
+                System.out.print(rownum+ " |");
+            }
+            else {
+                System.out.print(rownum+"|");
+            }
+            rownum++;
+
+            for( int j = 0; j< numberOfFields; j++){
+                if(field[i][j].isFlagged()){
+                    System.out.print("L ");
+                }
+                else if(field[i][j].isClicked()){
+                    System.out.print(field[i][j].getNeighborMines());
+                }
+                else{
+                    System.out.print("X ");
+                }
+            }
+            System.out.println("");
+        }
+    }
 
   private static void randomizeMines(Field[][] field){
         int randomx;
