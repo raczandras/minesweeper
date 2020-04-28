63480814fefc9cb0b426da305e2cabf61a16a382
Rácz András <raczandras0204@gmail.com>
2020-04-28 15:05:53 +0200

    Added a while cycle, now the game doesn't stop after the first user input.

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,26 +1,24 @@
 package main;
 
-import fieldSetup.Setup;
+import minefieldSetup.Setup;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import gameplayLogic.Print;
+import gameplayLogic.Printer;
 import gameplayLogic.Moves;
 
 public class Main {
 
     private static Logger logger = LoggerFactory.getLogger(Main.class);
-    private static int numberOfFields;
-
 
     public static void main(String[] args){
         logger.trace("Please type in the difficulty you want. easy or normal or hard");
         Setup.selectDifficulty();
-        numberOfFields = Setup.getNumberOfFields();
+        int numberOfFields = Setup.getNumberOfFields();
 
         Field[][] field = new Field[numberOfFields][numberOfFields];
 
         for(int i = 0; i < numberOfFields; i++){
-            for( int j = 0; j < numberOfFields; j++){
+            for(int j = 0; j < numberOfFields; j++){
                 field[i][j] = new Field();
             }
         }
@@ -28,11 +26,12 @@ public class Main {
         Setup.randomizeMines(field);
         Setup.setNeighbours(field);
 
-        Print.print(field, numberOfFields);
+        Printer.duringGamePrint(field, numberOfFields);
 
+        while(true){
+            Moves.userMove(field, numberOfFields);
+        }
     }
-
-
 
 }
 
