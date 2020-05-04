65c4ceef91791b8e829cda81cd2a2938ffabd3c8
Rácz András <raczandras0204@gmail.com>
2020-05-04 12:28:03 +0200

    Moved the code that was dealing with parts of the gameplay to the gameplay class.

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -3,19 +3,19 @@ package main;
 import gameplayLogic.FieldSetup;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import gameplayLogic.Printer;
-import gameplayLogic.Moves;
+import gameplayLogic.gameplay;
+import java.util.Scanner;
 
 public class Main {
 
     private static final Logger logger = LoggerFactory.getLogger(Main.class);
 
     public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
         logger.trace("Please type in the difficulty you want. easy or normal or hard");
         FieldSetup.selectDifficulty();
         int numberOfFields = FieldSetup.getNumberOfFields();
-        int numberOfMines = FieldSetup.getNumberOfMines();
-        int numberOfNotClicked = 0;
+        String result;
 
         Field[][] field = new Field[numberOfFields][numberOfFields];
 
@@ -28,25 +28,15 @@ public class Main {
         FieldSetup.randomizeMines(field);
         FieldSetup.setNeighbours(field);
 
-        Printer.duringGamePrint(field, numberOfFields);
+        result = gameplay.gamePlay(field);
 
-        while(true){
-            numberOfNotClicked = 0;
-            Moves.userMove(field, numberOfFields);
-
-            for(int i = 0; i < numberOfFields; i++){
-                for(int j = 0; j < numberOfFields; j++){
-                    if( !field[i][j].isClicked() ){
-                        numberOfNotClicked++;
-                    }
-                }
-            }
-            if( numberOfNotClicked == numberOfMines ){
-                Printer.endgamePrint(field, numberOfFields);
-                System.out.println("You won!");
-                break;
-            }
+        if(result.equalsIgnoreCase("win")){
+            System.out.println("You won!");
         }
+        else{
+            System.out.println("You lost!");
+        }
+
         System.exit(0);
     }
 
