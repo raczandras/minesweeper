38d2632e10d6a43390d5513f1417a0ed93d1cb14
Rácz András <raczandras0204@gmail.com>
2020-05-05 13:54:04 +0200

    Added annotations needed for JPA.

--- src/main/java/gameResult/Result.java
+++ src/main/java/gameResult/Result.java
@@ -1,8 +1,21 @@
 package gameResult;
 
-@lombok.Data
+import javax.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
 public class Result {
+        @Id
+        @GeneratedValue
+        private Long id;
+
+        @Column(nullable = false)
         private String name;
+
+        @Column(nullable = false)
         private String startingDate;
+
+        @Column(nullable = false)
         private float secondsTaken;
 }
