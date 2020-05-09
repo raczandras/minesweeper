fa3a0ca00ac7392482aa8e34026644d47e3e34b6
Rácz András <raczandras0204@gmail.com>
2020-05-09 17:11:05 +0200

    Switched out every onAction to onMouseClicked.

--- src/main/java/controller/gameplayController.java
+++ src/main/java/controller/gameplayController.java
@@ -16,35 +16,120 @@ public class gameplayController {
     private List<List<Button>> buttonsList;
 
     public void clickAction(MouseEvent actionEvent){
-        Field[][] field = FieldSetup.getField();
         for( int i = 0; i < buttonsList.size(); i++){
             for( int j = 0; j< buttonsList.get(i).size(); j++){
                 if( actionEvent.getSource().equals(buttonsList.get(i).get(j))){
                     if(actionEvent.getButton() == MouseButton.SECONDARY ){
-                        buttonsList.get(i).get(j).setText("F");
+                        if( FieldSetup.flagAField(i,j)){
+                            decideState();
+                        }
                     }
-                    else {
-                        buttonsList.get(i).get(j).setText(String.valueOf(field[i][j].getNeighborMines()));
-                        break;
+                    else if(FieldSetup.openAField(i,j)) {
+                            decideState();
                     }
+                    break;
                 }
             }
         }
     }
 
     public void forfeitAction(ActionEvent actionEvent){
-        System.exit(0);
+        lost();
     }
 
-    public void printField(){
+    public void decideState(){
+        if( didwin() ){
+            won();
+        }
+        else if( didlost() ){
+            lost();
+        }
+        else{
+            printState();
+        }
+    }
+
+    private void printState() {
         Field[][] field = FieldSetup.getField();
 
         for( int i = 0; i < buttonsList.size(); i++){
             for( int j = 0; j< buttonsList.get(i).size(); j++){
-                if(field[i][j].isClicked() ){
+                if(field[i][j].isFlagged()){
+                    buttonsList.get(i).get(j).setText("F");
+                }
+                else if(field[i][j].isClicked() ){
                     buttonsList.get(i).get(j).setText(String.valueOf(field[i][j].getNeighborMines()));
+                }
+                else{
+                    buttonsList.get(i).get(j).setText("");
                 }
             }
         }
+    }
+
+    private boolean didlost() {
+        Field[][] field = FieldSetup.getField();
+        for( int i = 0; i < buttonsList.size(); i++){
+            for( int j = 0; j< buttonsList.get(i).size(); j++){
+                if(field[i][j].isMine() && field[i][j].isClicked()){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    private boolean didwin() {
+        Field[][] field = FieldSetup.getField();
+        int numberOfClicked = 0;
+        int numberOfMines = FieldSetup.getNumberOfMines();
+
+        for(int i = 0; i < buttonsList.size(); i++){
+            for(int j = 0; j < buttonsList.get(i).size(); j++){
+                if( field[i][j].isClicked() ){
+                    numberOfClicked++;
+                }
+            }
+        }
+        if( numberOfClicked == (buttonsList.size()*buttonsList.size() - numberOfMines) ){
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    public void lost(){
+        Field[][] field = FieldSetup.getField();
+
+        for( int i = 0; i < buttonsList.size(); i++){
+            for( int j = 0; j< buttonsList.get(i).size(); j++){
+                if(field[i][j].isMine()){
+                    buttonsList.get(i).get(j).setText("M");
+                }
+                else{
+                    buttonsList.get(i).get(j).setText(String.valueOf(field[i][j].getNeighborMines()));
+                }
+                buttonsList.get(i).get(j).setDisable(true);
+            }
+        }
+        System.out.println("You lost!");
+    }
+
+    public void won(){
+        Field[][] field = FieldSetup.getField();
+
+        for( int i = 0; i < buttonsList.size(); i++){
+            for( int j = 0; j< buttonsList.get(i).size(); j++){
+                if(field[i][j].isMine()){
+                    buttonsList.get(i).get(j).setText("M");
+                }
+                else{
+                    buttonsList.get(i).get(j).setText(String.valueOf(field[i][j].getNeighborMines()));
+                }
+                buttonsList.get(i).get(j).setDisable(true);
+            }
+        }
+        System.out.println("You Won");
     }
 }
