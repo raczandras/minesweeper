28f617f03cf8fa816fc90af3d5c2ce371e1c15ea
Rácz András <raczandras0204@gmail.com>
2020-05-09 16:47:45 +0200

    Started the printField function that will print out the field to the GUI.

--- src/main/java/controller/gameplayController.java
+++ src/main/java/controller/gameplayController.java
@@ -4,6 +4,8 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import java.util.List;
+import main.Field;
+import gameplayLogic.FieldSetup;
 
 public class gameplayController {
 
@@ -11,10 +13,11 @@ public class gameplayController {
     private List<List<Button>> buttonsList;
 
     public void clickAction(ActionEvent actionEvent){
+        Field[][] field = FieldSetup.getField();
         for( int i = 0; i < buttonsList.size(); i++){
             for( int j = 0; j< buttonsList.get(i).size(); j++){
                 if( actionEvent.getSource().equals(buttonsList.get(i).get(j))){
-                    buttonsList.get(i).get(j).setText("K");
+                    buttonsList.get(i).get(j).setText(String.valueOf(field[i][j].getNeighborMines()));
                     break;
                 }
             }
@@ -23,5 +26,17 @@ public class gameplayController {
 
     public void forfeitAction(ActionEvent actionEvent){
         System.exit(0);
+    }
+
+    public void printField(){
+        Field[][] field = FieldSetup.getField();
+
+        for( int i = 0; i < buttonsList.size(); i++){
+            for( int j = 0; j< buttonsList.get(i).size(); j++){
+                if(field[i][j].isClicked() ){
+                    buttonsList.get(i).get(j).setText(String.valueOf(field[i][j].getNeighborMines()));
+                }
+            }
+        }
     }
 }
