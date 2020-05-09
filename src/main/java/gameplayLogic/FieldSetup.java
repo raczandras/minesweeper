02c087a9ea701f6b7b2f080f1244650ac7b10db4
Rácz András <raczandras0204@gmail.com>
2020-05-09 16:48:44 +0200

    Modified some functions, because now the program is working with a gui instead of a console.

--- src/main/java/gameplayLogic/FieldSetup.java
+++ src/main/java/gameplayLogic/FieldSetup.java
@@ -1,20 +1,59 @@
 package gameplayLogic;
 
+import controller.launchController;
 import main.Field;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.Scanner;
 
 public class FieldSetup {
 
     private static int numberOfFields;
     private static int numberOfMines;
+    public static Field[][] field;
+    private static final Logger logger = LoggerFactory.getLogger(FieldSetup.class);
 
-    public static int getNumberOfMines() { return numberOfMines; }
+    public static Field[][] getField() {
+        return field;
+    }
+
+    public static void setField(Field[][] field) {
+        FieldSetup.field = field;
+    }
+
     public static int getNumberOfFields() {
         return numberOfFields;
     }
 
-    public static void randomizeMines(Field[][] field){ //Assigns the mines randomly on the minefield
+    public static int getNumberOfMines() {
+        return numberOfMines;
+    }
 
+    public static void setNumberOfMines(int numberOfMines) {
+        FieldSetup.numberOfMines = numberOfMines;
+    }
+
+    public static void setNumberOfFields(int numberOfFields) {
+        FieldSetup.numberOfFields = numberOfFields;
+    }
+
+    public static void initField(int numberOfFields, int numberOfMines){
+        FieldSetup.numberOfFields = numberOfFields;
+        FieldSetup.numberOfMines = numberOfMines;
+        field = new Field[numberOfFields][numberOfFields];
+
+        for(int i = 0; i < numberOfFields; i++){
+            for(int j = 0; j < numberOfFields; j++){
+                field[i][j] = new Field();
+            }
+        }
+
+        randomizeMines();
+    }
+
+    public static void randomizeMines(){ //Assigns the mines randomly on the minefield
+        logger.trace("randomizeMines() started");
         for( int i = 0; i < numberOfMines; i++){
             int randomx = (int)(Math.random() * numberOfFields);
             int randomy = (int)(Math.random() * numberOfFields);
@@ -26,6 +65,8 @@ public class FieldSetup {
                 field[randomx][randomy].setMine(true);
             }
         }
+        logger.trace("adding mines finished");
+        setNeighbours();
     }
 
     public static void selectDifficulty(){ //Makes a player select a difficulty setting before the start of the game
@@ -53,7 +94,7 @@ public class FieldSetup {
         }
     }
 
-    public static void setNeighbours(Field[][] field){
+    private static void setNeighbours(){
 
         for( int i = 0; i < field.length; i++){
             for( int j = 0; j< field.length; j++){
@@ -68,5 +109,48 @@ public class FieldSetup {
                 }
             }
         }
+    }
+
+    public static int[][] getNeighboursIndexes(int row, int col){
+        int[][] neighbours;
+        int rowcount = 0;
+
+        for(int colNum = col - 1; colNum <= (col + 1); colNum++  ) {
+            for (int rowNum = row - 1; rowNum <= (row + 1); rowNum++  ) {
+                if(!((colNum == col) && (rowNum == row))) {
+                    if(withinGrid (colNum, rowNum)) {
+                        rowcount++;
+
+                    }
+                }
+            }
+        }
+        neighbours = new int[rowcount][2];
+        rowcount = 0;
+
+        for(int colNum = col - 1; colNum <= (col + 1); colNum++  ) {
+            for (int rowNum = row - 1; rowNum <= (row + 1); rowNum++  ) {
+                if(!((colNum == col) && (rowNum == row))) {
+                    if(withinGrid (colNum, rowNum)) {
+                        neighbours[rowcount][0] = rowNum;
+                        neighbours[rowcount][1] = colNum;
+                        rowcount++;
+                    }
+                }
+            }
+        }
+
+        return neighbours;
+    }
+
+    public static boolean withinGrid(int colNum, int rowNum) {
+
+        if((colNum < 0) || (rowNum <0) ) {
+            return false;
+        }
+        if((colNum >= numberOfFields) || (rowNum >= numberOfFields)) {
+            return false;
+        }
+        return true;
     }
 }
