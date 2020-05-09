eafc2861ab7b2ab68b29856d262b32adb43d7a9a
Rácz András <raczandras0204@gmail.com>
2020-05-09 17:10:44 +0200

    Switched out the actionEvent to a mouseEvent.

--- src/main/java/controller/gameplayController.java
+++ src/main/java/controller/gameplayController.java
@@ -3,6 +3,9 @@ package controller;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+
+import javafx.scene.input.MouseButton;
+import javafx.scene.input.MouseEvent;
 import java.util.List;
 import main.Field;
 import gameplayLogic.FieldSetup;
@@ -12,13 +15,18 @@ public class gameplayController {
     @FXML
     private List<List<Button>> buttonsList;
 
-    public void clickAction(ActionEvent actionEvent){
+    public void clickAction(MouseEvent actionEvent){
         Field[][] field = FieldSetup.getField();
         for( int i = 0; i < buttonsList.size(); i++){
             for( int j = 0; j< buttonsList.get(i).size(); j++){
                 if( actionEvent.getSource().equals(buttonsList.get(i).get(j))){
-                    buttonsList.get(i).get(j).setText(String.valueOf(field[i][j].getNeighborMines()));
-                    break;
+                    if(actionEvent.getButton() == MouseButton.SECONDARY ){
+                        buttonsList.get(i).get(j).setText("F");
+                    }
+                    else {
+                        buttonsList.get(i).get(j).setText(String.valueOf(field[i][j].getNeighborMines()));
+                        break;
+                    }
                 }
             }
         }
