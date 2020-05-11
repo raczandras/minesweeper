9d75b07c80496f6fb5e29a0730af0ad2a5e77cf3
Rácz András <raczandras0204@gmail.com>
2020-05-11 15:10:08 +0200

    Removed the logger from the main class.

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,12 +1,8 @@
 package main;
 
 import javafx.application.Application;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class Main {
-
-    private static final Logger logger = LoggerFactory.getLogger(Main.class);
 
     public static void main(String[] args){
         Application.launch(MyApplication.class, args);
