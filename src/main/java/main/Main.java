d8c6634c26e6eebd8fbbacc2aa12d4b0e120b3f6
Rácz András <raczandras0204@gmail.com>
2020-05-11 14:49:18 +0200

    Removed the now unnecessary code from main.

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,12 +1,8 @@
 package main;
 
-import gameplayLogic.FieldSetup;
 import javafx.application.Application;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import gameplayLogic.gameplay;
-import gameResult.Leaderboard;
-import java.time.LocalDateTime;
 
 public class Main {
 
@@ -14,41 +10,6 @@ public class Main {
 
     public static void main(String[] args){
         Application.launch(MyApplication.class, args);
-
-        /*logger.trace("Please type in the difficulty you want. easy or normal or hard");
-        FieldSetup.selectDifficulty();
-        int numberOfFields = FieldSetup.getNumberOfFields();
-        String end;
-        LocalDateTime startingDate;
-        long startTime;
-        long endTime;
-
-        Field[][] field = new Field[numberOfFields][numberOfFields];
-
-        for(int i = 0; i < numberOfFields; i++){
-            for(int j = 0; j < numberOfFields; j++){
-                field[i][j] = new Field();
-            }
-        }
-
-        FieldSetup.randomizeMines(field);
-        FieldSetup.setNeighbours(field);
-        startingDate = LocalDateTime.now();
-        startTime = System.currentTimeMillis();
-        end = gameplay.gamePlay(field);
-
-        if(end.equalsIgnoreCase("win")){
-            System.out.println("You won!");
-            endTime = System.currentTimeMillis();
-            Leaderboard.makeNewResult(startingDate, startTime, endTime);
-        }
-        else{
-            System.out.println("You lost!");
-            endTime = System.currentTimeMillis();
-            Leaderboard.makeNewResult(startingDate, startTime, endTime);
-        }
-
-        System.exit(0);*/
     }
 
 }
